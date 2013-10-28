bc0b170e8b47f3e13eedf41ac3212b69fa014b78
kskiss <kongshuai@kongshuai.local>
2013-10-28 17:12:54 +0800

    create new project

---
+++ Caculator/src/com/example/views/MyButton.java
@@ -0,0 +1,28 @@
+package com.example.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+//import android.widget.Button;
+
+public class MyButton extends Button{
+	public MyButton(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyButton(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int tag;
+
+	public MyButton(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
